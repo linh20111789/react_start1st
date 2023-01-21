260e5f47d6df1d7dfa8a25749bcec07dd7a63e07
unknown <linh20111789@gmail.com>
2023-01-21 14:42:47 +0700

    Task 1: thay logo, thay hyperlink, không cho ảnh xoay

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './img-avatar.jpg';
 import './App.css';
 
 function App() {
@@ -6,16 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/linh20111789"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My name is Nguyen Van Linh
         </a>
       </header>
     </div>
